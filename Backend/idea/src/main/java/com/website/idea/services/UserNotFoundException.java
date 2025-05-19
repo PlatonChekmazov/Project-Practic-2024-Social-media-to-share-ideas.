ff638899850281270706cc89424b928738fa7360
Platon Chekmazov <94749714+PlatonChekmazov@users.noreply.github.com>
2025-05-19 08:56:38 +0300

    Delete Backend directory

--- Backend/idea/src/main/java/com/website/idea/services/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.website.idea.services;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
